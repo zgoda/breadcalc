334f41f44bd0cd7bc70e1d556768d19234b6792b
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-08-19 18:23:53 +0200

    Are we back?

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -3,6 +3,12 @@ const AmountType = Object.freeze({
   PERCENT: 'percent',
 });
 
+/**
+ * Round number to specified precision
+ * @param {Number} num number to round
+ * @param {Number} dp decimal places
+ * @returns Number
+ */
 function round(num, dp) {
   const multiplier = 10 ** dp;
   return Math.round((num + Number.EPSILON) * multiplier) / multiplier;
