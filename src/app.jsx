8e989d0b4bee493cae8abf04af43b84e06df5053
Jarek Zgoda <jarek.zgoda@gmail.com>
2021-11-15 16:33:07 +0100

    Even simpler

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,3 @@
-import { Navigation } from './components/navigation';
 import { PageInfo, SectionTitle } from './components/pageinfo';
 import { CalcStart } from './components/calcstart';
 import { DryIngredients } from './components/dryingredients';
@@ -12,19 +11,20 @@ import './style/index.scss';
 
 export function App() {
   return (
-    <div class="container">
-      <Navigation />
-      <PageInfo title="Kalkulator ciasta chlebowego" />
-      <CalcStart />
-      <SectionTitle title={'Składniki'} level={2} />
-      <DryIngredients />
-      <WetIngredients />
-      <DryAdjuncts />
-      <WetAdjuncts />
-      <Leaven />
-      <SectionTitle title={'Ciasto właściwe'} level={2} />
-      <hr />
+    <>
+      <div class="container">
+        <PageInfo title="Kalkulator ciasta chlebowego" />
+        <CalcStart />
+        <SectionTitle title={'Składniki'} level={2} />
+        <DryIngredients />
+        <WetIngredients />
+        <DryAdjuncts />
+        <WetAdjuncts />
+        <Leaven />
+        <SectionTitle title={'Ciasto właściwe'} level={2} />
+        <hr />
+      </div>
       <About />
-    </div>
+    </>
   );
 }
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,25 +2,21 @@ import json from './about.json';
 
 function About() {
   return (
-    <>
+    <footer class="container">
       {json.text.map((line, index) => (
         <p key={`about-line-${index}`}>{line}</p>
       ))}
-      <div class="footerlinks">
-        <ul>
-          {json.links.map((item, index) => {
-            const [title, url] = [...item];
-            return (
-              <li key={`link-${index}`}>
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                  {title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <div class="row">
+        {json.links.map((item, index) => {
+          const [title, url] = item;
+          return (
+            <div key={`link-${index}`} class="column center">
+              <a href={url}>{title}</a>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </footer>
   );
 }
 
